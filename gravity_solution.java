ee36620d3277daf4ace472fbe35f22cb0c959e7a
nimisha026 <49266248+nimisha026@users.noreply.github.com>
2019-08-20 23:11:42 +0530

    Update gravity_solution.java
    commit initial

--- gravity_solution.java
+++ gravity_solution.java
@@ -3,9 +3,9 @@ class gravity_solution {
   public static void main(String[] args) {
     System.out.println("Enter Time");
     Scanner Sc = new Scanner(System.in);
-    double time = Sc.nextDouble();
-    double g = 9.8;
-    double velocity = 0.5*(g*time*time);
-    System.out.println("The speed of the object at "+ time +" seconds after its release is "+ velocity +" and the distance the object has travelled in the "+ time +" seconds after the relase is "+ g*time );
+    double t = Sc.nextDouble();
+    double grav = 9.8;
+    double velo = 0.5*(grav*t*t);
+    System.out.println("The speed of the object at "+ t +" seconds after its release is "+ velo +" and the distance the object has travelled in the "+ time +" seconds after the relase is "+ g*time );
     }
 }
