1a80ee2803c487e1c02ebf622650e12e3a93e8c7
Nimisha Pachauri <49266248+nimisha026@users.noreply.github.com>
2019-08-22 00:08:14 +0530

    Update gravity_solution.java
    commit initialise

--- gravity_solution.java
+++ gravity_solution.java
@@ -6,6 +6,6 @@ class gravity_solution {
     double t = Sc.nextDouble();
     double grav = 9.8;
     double velo = 0.5*(grav*t*t);
-    System.out.println("The speed of the object at "+ t +" seconds after its release is "+ velo +" and the distance the object has travelled in the "+ time +" seconds after the relase is "+ g*time );
+    System.out.println("The speed of the object at "+ t +" seconds after its release is "+ velo +" and the distance the object has travelled in the "+ t +" seconds after the relase is "+ grav*t );
     }
 }
